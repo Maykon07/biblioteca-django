1bf76d07f623346fcd3eabaf35cf7cdf95fb793e
Maykon Santos <maykonsantos215@gmail.com>
2024-10-08 17:55:15 -0300

    versão 2

--- biblioteca/core/views.py
+++ biblioteca/core/views.py
@@ -1,3 +1,4 @@
+'''
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
@@ -5,7 +6,81 @@ from rest_framework import status
 from .models import Livro
 from .serializers import LivroSerializer
 from rest_framework.decorators import api_view
+'''
 
+# reimplemente as views utilizando class-based views em vez de function-based views.
+
+from rest_framework import generics
+
+from biblioteca.core.filters import LivroFilter
+from .models import Livro
+from .serializers import LivroSerializer
+from .models import Autor
+from .serializers import AutorSerializer
+from .models import Categoria
+from .serializers import CategoriaSerializer
+from django_filters import rest_framework as filters
+
+# Implemente filtros de busca para o atributo nome, utilizando o prefixo ^ (inicia com), para Categoria e Livro.
+
+class LivroList(generics.ListCreateAPIView):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
+    name = "livro-list"
+    filterset_fields = ('titulo', 'autor', 'categoria', 'publicado_em')
+    search_fields = (
+        '^titulo',
+        '^autor__nome',
+        '^categoria__nome',
+        '^publicado_em'
+    )
+    ordering_fields = ('titulo', 'autor', 'categoria', 'publicado_em')
+#A view LivroList deve permitir a ordenação por titulo, autor, categoria, publicado_em.
+
+
+class LivroDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
+    name = "livro-detail"
+
+#devem ser replicados também para Categoria e Autor.
+#Possibilite a ordenação a partir do atributo nome para Categoria e Livro.
+
+class CategoriaList(generics.ListCreateAPIView):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+    name = "categoria-list"
+    filterset_fields = ('nome',)
+    search_fields = (
+        '^nome',
+    )
+    ordering_fields = ('nome',)
+
+class CategoriaDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+    name = "categoria-detail"
+
+
+
+class AutorList(generics.ListCreateAPIView):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+    name = "autor-list"
+    filterset_fields = ('nome',)
+    search_fields = (
+        '^nome',
+    )
+    ordering_fields = ('nome',)
+
+class AutorDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+    name = "autor-detail"
+
+
+
+'''
 # Create your views here.
 @api_view(['GET','POST'])
 @csrf_exempt
@@ -39,3 +114,4 @@ def livro_detail(request, pk):
     if request.method == 'DELETE':
         livro.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+'''
